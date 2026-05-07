4cb2481da76adb29a96dec774afdff3276e85d8f
Ong Kai Jie <youremail@gmail.comjeffrey975522@outlook.com>
2026-05-07 18:19:01 +0800

    fix stripe api version

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' })
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2026-04-22.dahlia' })
 
 export async function POST(req: NextRequest) {
   try {
